896fbf9613b6626a7ea8af71e5ab96b673483040
Lizeth Paiva <lizethpaiva2@gamil.com>
2024-11-30 16:21:15 -0500

    Correo

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
       </ul>
       <ul className='flex items-center gap-3'>
         <li className='text-black/60'>
-          Liz@Paiva.com
+          Liz@Paiva.com.
         </li>
         <li>
           <NavLink
